e8080e08303ea7fbe0c9ad5fef9a9bb4b7336832
haputy <andrewpharvey@gmail.com>
2020-12-20 12:48:49 -0600

    Create page.php
    initialcommit

---
+++ page.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=Noto+Sans+JP&family=Russo+One&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap" rel="stylesheet">
+    <title>Zoom Dance Experiment</title>
+    <style>body{background-color:#000}.nomargin{margin-block-end:0}.fira{font-family: 'Fira Sans', sans-serif; text-align:center;}.jetbrains{font-family:'JetBrains Mono', monospace; text-align:center;}.ibm{font-family:'IBM Plex Mono',monospace;text-align:center}.russo{font-family:'Russo One',sans-serif;text-align:center}.noto: {font-family:'Noto Sans JP',sans-serif;text-align:center}.font28{font-size:28pt}.font32{font-size:32pt}.font24{font-size:24pt}.green{color:#daffd8}.black{color:#000}.font23{font-size:23pt}.font17{font-size:17pt}.font19{font-size:19pt}.font20{font-size:20pt}a{color:#00f}</style>
+</head>
+<body>
+    <div class="center">
+	<br><br>
+	<!-- <div class="green font19 fira" style="text-align:center">page, <a href="http://zoom.com">click</a>, open, dancing</div><br><br><br><br><br><br><br> -->
+<p id="demo" class="green font32 jetbrains font28 nomargin" style="text-align:center"></p>
+<div class="green font19 fira" style="text-align:center">until...</div><br><br><br><br><br><br><br>
+	<div class="ibm font24 green center">The</div>
+    <div class="russo font32 green">Zoom Dance Experiment</div>
+        <div class="ibm green font23">12/31/20 ❖ 8:30 p.m.</div>
+        <div class="ibm font19"><a href="https://us02web.zoom.us/j/81588181351?pwd=WVE2L21NT3pCOUozSFFVZTVGdmJJQT09" target="_blank">JOIN THE CALL</a></div><br>
+        <div class="ibm font20 green">or watch on:</div><br>
+        <div class="ibm font19 green"><a href="https://www.youtube.com/channel/UCv4BIgLV-18S4vaH1A_Xdhg" target="_blank">YouTube</a> | <a href="http://facebbook.com/zde2020" target="_blank">Facebook</a> | <a href="https://www.instagram.com/zoom_dance_experiment/" target="_blank">Instagram</a> | <a href="https://www.twitch.tv/zoom_dance_experiment" target="_blank">Twitch</a></div>
+</p>
+</div>
+
+<script>var countDownDate=new Date("December 20, 2020 18:30:00").getTime();var x=setInterval(function(){var now=new Date().getTime();var distance=countDownDate-now;var days=Math.floor(distance/(1000*60*60*24));var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));var minutes=Math.floor((distance%(1000*60*60))/(1000*60));var seconds=Math.floor((distance%(1000*60))/1000);document.getElementById("demo").innerHTML=days+"d "+hours+"h "+minutes+"m "+seconds+"s ";if(distance<0){clearInterval(x);document.getElementById("demo").innerHTML="EXPIRED";}},1000);</script>
+</html>
